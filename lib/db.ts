414078f077f4f0abd241687bf9173a13073afe78
Evan Castella <ecastella@Evans-MacBook-Pro.local>
2025-01-24 16:19:59 -0700

    db.ts, eslintrc.json changed

--- lib/db.ts
+++ lib/db.ts
@@ -1,13 +1,9 @@
 import { PrismaClient } from "@prisma/client"
 
 declare global {
-  namespace NodeJS {
-    interface Global {
-      prisma: PrismaClient | undefined
-    }
-  }
+  var prisma: PrismaClient | undefined
 }
 
-export const db = global.prisma || new PrismaClient()
+export const db = globalThis.prisma || new PrismaClient()
 
-if (process.env.NODE_ENV !== "production") global.prisma = db 
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db 
