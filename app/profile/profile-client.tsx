868126d41570f72f6930047153011a6e094f5c7f
Evan Castella <ecastella@Evans-MacBook-Pro.local>
2025-01-25 14:48:08 -0700

    middleware updated

--- app/profile/profile-client.tsx
+++ app/profile/profile-client.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import  Profile  from '@prisma/client';
+import  Profile from '@prisma/client';
 import  profileFormSchema  from '@/lib/validations/profile';
 import axios from 'axios';
 import { toast } from 'react-hot-toast';
