cf6e40e86f51f54d877d76e20f15eaa6ef634503
Evan Castella <ecastella@Evans-MacBook-Pro.local>
2025-01-31 19:02:44 -0700

    Stripe.ts/route.ts changed

--- app/api/cancel-subscription/route.ts
+++ app/api/cancel-subscription/route.ts
@@ -1,11 +1,11 @@
-import { stripe } from '../../../lib/stripe';
 import { NextResponse } from 'next/server'
+import { stripe } from '@/lib/stripe'
 
 export async function POST(req: Request) {
   try {
     const { subscriptionId } = await req.json()
     
-    const canceledSubscription = await stripe.subscriptions.del(subscriptionId)
+    const canceledSubscription = await stripe.subscriptions.cancel(subscriptionId)
     return NextResponse.json(canceledSubscription)
   } catch (error) {
     console.error('Error:', error)
--- lib/stripe.ts
+++ lib/stripe.ts
@@ -2,7 +2,7 @@ import Stripe from 'stripe';
 import { db } from '@/lib/db';
 import { buffer } from 'micro';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2024-12-18.acacia',
 });
 
