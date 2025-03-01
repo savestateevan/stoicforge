ad7e48528587d89fcdc602ab79106260d7892d7c
Evan Castella <ecastella@Evans-MacBook-Pro.local>
2025-02-28 19:21:27 -0700

    updated webhook

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -7,6 +7,10 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2025-01-27.acacia'
 });
 
+// This tells Next.js to not parse the request body
+export const dynamic = 'force-dynamic';
+export const bodyParser = false;
+
 export async function POST(req: Request) {
   const body = await req.text();
   const signature = headers().get('Stripe-Signature')!;
@@ -76,9 +80,3 @@ export async function POST(req: Request) {
     );
   }
 }
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
