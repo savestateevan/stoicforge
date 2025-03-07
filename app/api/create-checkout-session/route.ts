b7ad5e72b3e6f53d57aad0df800f2fc337c9ac95
Evan Castella <ecastella@Evans-MacBook-Pro.local>
2025-03-07 13:59:02 -0700

    Webhook fixed hopefully?

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -83,7 +83,7 @@ export async function POST(req: NextRequest) {
       
       billing_address_collection: 'auto',
       
-      success_url: `${process.env.NEXT_PUBLIC_URL}/dashboard?success=true`,
+      success_url: `${process.env.NEXT_PUBLIC_URL}/pricing?success=true`,
       cancel_url: `${process.env.NEXT_PUBLIC_URL}/pricing?canceled=true`,
       
       subscription_data: {
