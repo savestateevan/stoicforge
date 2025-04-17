7561dc28a360f8aa86b432d37c8e1d8878510071
Evan Castella <ecastella@Evans-MacBook-Pro.local>
2025-04-17 13:58:19 -0700

    Metadata updated again

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,4 +1,12 @@
 import { SignUp } from '@clerk/nextjs'
+import { Metadata } from 'next';
+
+
+export const metadata: Metadata = {
+  title: 'Create Your StoicForge Account',
+  description:
+    'Start chatting with Marcus Aurelius & Seneca in minutes – no card required.',
+};
 
 export default function Page() {
   return (
